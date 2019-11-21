586d8f8c7e3871168dfe707e320fb556edca62d7
SahilU <sahilu.99@gmail.com>
2019-11-20 21:24:34 -0800

    updated the bar chart title

--- ui.R
+++ ui.R
@@ -2,7 +2,8 @@ library(shiny)
 library(plotly)
 
 shinyUI(fluidPage(
-  titlePanel("Bar Chart"),
+  titlePanel("Comparing Employment Rate Between Those with Disablities 
+              and Those Without"),
     mainPanel(
       plotlyOutput("maingraph")
     )
